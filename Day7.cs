8df926a076875b79b8ea2770ffc3b3acf16f862e
ukmukfukk <ukmukfukk@gmail.com>
2020-12-06 09:50:32 +0100

    Day 7 prep

---
+++ Day7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public class Day7 : IDay
+    {
+        public IList<string> InputFiles => new List<string> { "input7.txt." };
+
+        public string Name => "Day 7";
+
+        public void SolvePuzzle1(IList<IList<string>> inputs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SolvePuzzle2(IList<IList<string>> inputs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
