a1e70f478b34d2a02e5ac7605204845be3ad7002
ukmukfukk <ukmukfukk@gmail.com>
2020-12-13 06:00:32 +0100

    Day 13 prep

---
+++ Day13.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public class Day13 : IDay
+    {
+        public IList<string> InputFiles => new List<string> { "input13test.txt", "input13.txt" };
+
+        public string Name => "Day 13";
+
+        public void SolvePuzzle1(IList<IList<string>> inputs)
+        {
+            foreach (var input in inputs)
+            {
+                SolvePuzzle1(input);
+            }
+        }
+
+        public void SolvePuzzle2(IList<IList<string>> inputs)
+        {
+            foreach (var input in inputs)
+            {
+                SolvePuzzle2(input);
+            }
+        }
+
+        private void SolvePuzzle1(IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SolvePuzzle2(IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
