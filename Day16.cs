a289c1b97765f1a7b6867eb44d60faf55beb78bd
ukmukfukk <ukmukfukk@gmail.com>
2020-12-15 17:57:22 +0100

    Day 16 prep

---
+++ Day16.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public class Day16 : SimpleDay
+    {
+        public override IList<string> InputFiles => new List<string> { "input16test.txt", "input16.txt" };
+
+        public override string Name => "Day 16";
+
+        protected override void SolvePuzzle1(IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SolvePuzzle2(IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
