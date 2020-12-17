3ff60f153d71f41cb5d125376aee792f895d46a9
ukmukfukk <ukmukfukk@gmail.com>
2020-12-17 17:45:56 +0100

    Day 18 prep

---
+++ AdventOfCode2020/Day18.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public class Day18 : SimpleDay
+    {
+        public override IList<string> InputFiles => new List<string> { "input18test.txt", "input18.txt" };
+
+        public override string Name => "Day 18";
+
+        protected override void SolvePuzzle1(IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SolvePuzzle2(IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
