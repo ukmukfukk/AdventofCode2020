2d1462758538a4864bb4deeb5e75c20bd07d034a
ukmukfukk <ukmukfukk@gmail.com>
2020-12-22 16:48:39 +0100

    Day 23 prep

---
+++ AdventOfCode2020/Day23.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020
+{
+    public class Day23 : DayUsingInputNames
+    {
+        public override IList<string> InputFiles => new List<string> { "input23test.txt", "input23.txt" };
+
+        public override string Name => "Day 23";
+
+        protected override void SolvePuzzle1(string inputname, IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SolvePuzzle2(string inputname, IList<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
